6b02ebfb2bde355656ea0c71391fa122bafd6713
scientechshub11 <56425994+scientechshub11@users.noreply.github.com>
2026-02-16 01:18:50 +0530

    added changes in db config

--- products/src/config/db.js
+++ products/src/config/db.js
@@ -5,13 +5,16 @@ const sequelize = new Sequelize(process.env.DB_NAME,process.env.POSTGRES_USER, p
     host: process.env.POSTGRES_HOST,
     port:process.env.DB_PORT,
     dialect:'postgres',
-    logging: false, // turn off noisy SQL logs (optional)
+    logging: true, // turn off noisy SQL logs (optional)
     pool: {
       max: 5,
       min: 0,
       acquire: 30000,
       idle: 10000
-    }
+    },
+    dialectOptions: process.env.DB_SSL === "true"
+      ? { ssl: { require: true, rejectUnauthorized: true } }
+      : {}
 })
 
 
